b03ab29eb8bc3c7b52eb8579ef91f81be76f0500
ShonBC <scortes3@umd.edu>
2021-04-08 20:56:07 -0700

    Added feature matching using brute force matcher.

--- stereo.py
+++ stereo.py
@@ -38,14 +38,35 @@ def sift(image): # Use SIFT to find keypoints and descriptors for image and draw
     key_points, descriptors = sift.detectAndCompute(gray, None)
     image = cv2.drawKeypoints(gray, key_points, image)    
 
-    return image
+    return key_points, descriptors
+
+def features(image_1, image_2): # Use keypoints and descriptors form SIFT to match features between two images
+
+    key_points_1, descriptors_1 = sift(image_1)
+    key_points_2, descriptors_2 = sift(image_2)
+
+
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(descriptors_1, descriptors_2, k=2)
+
+    good_match = []
+    for m, n in matches: # Apply ratio test to filter for good matches
+
+        if m.distance < 0.75 * n.distance:
+
+            good_match.append([m])
+
+    image_3 = cv2.drawMatchesKnn(image_1, key_points_1, image_2, key_points_2, good_match, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    return image_3
 
 if __name__ == "__main__":
 
-    im0, im1 = data2()
-    
-    sift(im0)
+    im0, im1 = data3()
+
+    img3 = features(im0, im1)
 
     cv2.imshow('img0', im0)
     cv2.imshow('img1', im1)
+    cv2.imshow('img3', img3)
     cv2.waitKey(0)
