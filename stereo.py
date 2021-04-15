1428c592148ffb2ba0eeb969561f693ea5b34012
ShonBC <scortes3@umd.edu>
2021-04-14 21:43:06 -0700

    Added function for computing homography for image rectification.  Moved camera parameters into data methods.

--- stereo.py
+++ stereo.py
@@ -6,13 +6,22 @@ Project 3 Stereo Vision System
 import cv2
 import numpy as np
 
+
 def data1(): # Initializes images from data set 1
 
     im0 = cv2.imread('data_1/im0.png')
     im0 = cv2.resize(im0, (720,480))
     im1 = cv2.imread('data_1/im1.png')
     im1 = cv2.resize(im1, (720,480))
-    return im0, im1
+
+    cam0 = np.array([[5299.313, 0, 1263.818],
+    [0, 5299.313, 977.763],
+    [0, 0, 1]])
+    cam1 = np.array([[5299.313, 0, 1438.004],
+    [0, 5299.313, 977.763],
+    [0, 0, 1]])
+
+    return im0, im1, cam0, cam1
 
 def data2(): # Initializes images from data set 2
 
@@ -21,7 +30,14 @@ def data2(): # Initializes images from data set 2
     im1 = cv2.imread('data_2/im1.png')
     im1 = cv2.resize(im1, (720,480))
 
-    return im0, im1
+    cam0 = np.array([[4396.869, 0, 1353.072],
+    [0, 4396.869, 989.702],
+    [0, 0, 1]])
+    cam1 = np.array([[4396.869, 0, 1538.86],
+    [0, 4396.869, 989.702],
+    [0, 0, 1]])
+
+    return im0, im1, cam0, cam1
 
 def data3(): # Initializes images from data set 3
 
@@ -29,7 +45,15 @@ def data3(): # Initializes images from data set 3
     im0 = cv2.resize(im0, (720,480))
     im1 = cv2.imread('data_3/im1.png')
     im1 = cv2.resize(im1, (720,480))
-    return im0, im1
+
+    cam0 = np.array([[5806.559, 0, 1429.219],
+    [0, 5806.559, 993.403],
+    [0, 0, 1]])
+    cam1 = np.array([[5806.559, 0, 1543.51],
+    [0, 5806.559, 993.403],
+    [0, 0, 1]])
+
+    return im0, im1, cam0, cam1
 
 def sift(image): # Use SIFT to find keypoints and descriptors for image and draw key points
 
@@ -45,7 +69,6 @@ def features(image_1, image_2): # Use keypoints and descriptors form SIFT to mat
     key_points_1, descriptors_1 = sift(image_1)
     key_points_2, descriptors_2 = sift(image_2)
 
-
     bf = cv2.BFMatcher()
     matches = bf.knnMatch(descriptors_1, descriptors_2, k=2)
 
@@ -56,11 +79,15 @@ def features(image_1, image_2): # Use keypoints and descriptors form SIFT to mat
     """
 
     good_match = []
+    pts_1 = []
+    pts_2 = []
     for m, n in matches: # Apply ratio test to filter for good matches
 
         if m.distance < 0.75 * n.distance:
 
             good_match.append([m])
+            # pts_1.append(key_points_1[m.trainIdx].pt)
+            # pts_2.append(key_points_2[n.trainIdx].pt)
 
     x1, y1 = [], []
     x2, y2 = [], []
@@ -69,15 +96,23 @@ def features(image_1, image_2): # Use keypoints and descriptors form SIFT to mat
 
         x1.append(key_points_1[good_match[i][0].queryIdx].pt[0])
         y1.append(key_points_1[good_match[i][0].queryIdx].pt[1])
+        pts_1.append(key_points_1[good_match[i][0].queryIdx].pt)
         
         x2.append(key_points_2[good_match[i][0].trainIdx].pt[0])
         y2.append(key_points_2[good_match[i][0].trainIdx].pt[1])
+        pts_2.append(key_points_2[good_match[i][0].trainIdx].pt)
 
     f = fun_mtx(x1, y1, x2, y2) # Fundamental Matrix
+    print(f)
+    F, mask = cv2.findFundamentalMat(np.int32(pts_1),np.int32(pts_2), cv2.FM_RANSAC)
+    print(F)
+
+    _, h1, h2 = cv2.stereoRectifyUncalibrated(np.int32(pts_1),np.int32(pts_2), f, (720,480))
+    _, H1, H2 = cv2.stereoRectifyUncalibrated(np.int32(pts_1),np.int32(pts_2), F, (720,480))
 
     image_3 = cv2.drawMatchesKnn(image_1, key_points_1, image_2, key_points_2, good_match, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-    E0, E1 = ess_mtx(f) # ESsential Matrix
+    E = ess_mtx(f) # ESsential Matrix
 
     return image_3, f
 
@@ -99,31 +134,30 @@ def fun_mtx(x1, y1, x2, y2): # Compute the Fundamental Matrix using matched key 
 
 def ess_mtx(fundamental_matrix): # Compute the Essential Matrix from the Fundamental Matrix
 
-    cam0 = np.array([[5299.313, 0, 1263.818],
-    [0, 5299.313, 977.763],
-    [0, 0, 1]])
-    cam1 = np.array([[5299.313, 0, 1438.004],
-    [0, 5299.313, 977.763],
-    [0, 0, 1]])
-
     w = np.array([[0, -1, 0],
     [1, 0, 0],
     [0, 0, 1]])
 
-    E0 = cam0.T * fundamental_matrix * cam0
-    E1 = cam1.T * fundamental_matrix * cam1
+    E = cam1.T * fundamental_matrix * cam0
 
-    u, s, v = np.linalg.svd(E0)
+    # Decompose essential matrix into rotation and translation
+    u, s, v = np.linalg.svd(E)
     c1 = u[-1]
     c2 = -u[-1]
     r1 = u * w * v.T
     r2 = u * w.T * v.T
 
-    return E0, E1
-    
+    return E
+
+def rectify(image_1, image_2, H1, H2):
+
+    cv2.initUndistortRectifyMap()
+
+    pass
+
 if __name__ == "__main__":
 
-    im0, im1 = data3() # Choose which data set to apply stereo vision to (Each data set consists of two images)
+    im0, im1, cam0, cam1 = data3() # Choose which data set to apply stereo vision to (Each data set consists of two images)
 
     img3, f = features(im0, im1) # Apply SIFT to match features and compute the fundamental matrix
 
